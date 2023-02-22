962d595bea8e21cba7617f98dc176e92bfebf2b9
khrismal <96888196+khrismal@users.noreply.github.com>
2023-02-21 20:07:05 -0500

    Update GitPracticeTwo.java
    this is for pull practice

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -4,7 +4,8 @@ public class GitPracticeTwo {
 
 	public static void main(String[] args) {
 		System.out.println("This is for push practice");
-		System.out.println("This is 2 practice")
+		System.out.println("This is 2 practice");
+		System.out.println("This is a practice for pull");
 	}
 
 }
